503edf6dc97625a1d9c4709caad9fe3301630769
Mathieu De Raedt <mathieu@deraedt.dev>
2021-11-04 12:41:41 +0100

    Intial checkin

---
+++ ResourceCalculator/Web/Program.cs
@@ -0,0 +1,25 @@
+using AntDesign.ProLayout;
+using Web.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+            builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings")); // From wwwroot/appsettings
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
